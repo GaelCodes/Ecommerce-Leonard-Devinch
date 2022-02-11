c908dae6cbd0e13fed4f407a764f4ecb9ecb9dbf
Abigael <abigael-hf@hotmail.com>
2022-02-11 14:35:02 +0100

    Refactorización del pdfsManager, se lee mejor así

--- codigo/backend/public/stripe-webhooks/webhooks-utilities/pdfsManager.php
+++ codigo/backend/public/stripe-webhooks/webhooks-utilities/pdfsManager.php
@@ -1,8 +1,17 @@
 <?php
 
+require_once __ROOT__ .
+  "/public/common-utilities/artworksDatabaseManagerClass.php";
+require_once __ROOT__ .
+  "/public/common-utilities/artworksDatabaseManagerClass.php";
+
 class PdfsManager
 {
   private string $output_file_name;
+  private string $output_dir;
+  private string $pdf_path;
+  private string $html_path;
+  private string $template_path = "tmp-files/order-template/order-template-page.php";
 
   public function __construct()
   {
@@ -15,21 +24,21 @@ class PdfsManager
     \Stripe\PaymentIntent $payment_intent
   ) {
     // Generación del nombre (el id de la order)
+    $this->output_dir = __DIR__ . "/tmp-files/generated-orders/";
     $this->output_file_name = $order->get_order_id();
+
+    $this->pdf_path = $this->output_dir . $this->output_file_name . ".pdf";
+    $this->html_path = $this->output_dir . $this->output_file_name . ".html";
 
     // Generación del archivo html a partir
     // de la ejecución del template php
     ob_start();
-    include "tmp-files/order-template/order-template-page.php";
-    $order_html_file = fopen(
-      __DIR__ .
-        "/tmp-files/generated-orders/" .
-        $this->output_file_name .
-        ".html",
-      "w"
-    );
-    fwrite($order_html_file, ob_get_clean());
-    fclose($order_html_file);
+    include $this->template_path;
+
+    $html_resource_id = fopen($this->html_path, "w");
+
+    fwrite($html_resource_id, ob_get_clean());
+    fclose($html_resource_id);
 
     // Ejecución del programa que genera el PDF
 
@@ -40,26 +49,32 @@ class PdfsManager
     */
 
     //
+    $bin_path = realpath(
+      __DIR__ . $_ENV["DIR_OF_WKHTMLTOPDF_FROM_WEBHOOK_UTILITIES"]
+    );
     $cmd =
       "\"" .
-      realpath(__DIR__ . $_ENV["DIR_OF_WKHTMLTOPDF_FROM_WEBHOOK_UTILITIES"]) .
+      $bin_path .
       "\" \"--enable-local-file-access\" " .
       "\"" .
-      realpath(__DIR__) .
-      "/tmp-files/generated-orders/{$this->output_file_name}.html" .
+      $this->html_path .
       "\" " .
       "\"" .
-      realpath(__DIR__) .
-      "/tmp-files/generated-orders/{$this->output_file_name}.pdf" .
+      $this->pdf_path .
       "\" ";
 
     /*
      Objective:
     
      " "webhooks-utilities/tmp-files/order-generator/bin/wkhtmltopdf.exe"
-       "template.html" "generated.pdf" "
+       "generated.html" "generated.pdf" "
     */
 
     exec($cmd);
+  }
+
+  public function get_pdf_path(): string
+  {
+    return $this->pdf_path;
   }
 }
