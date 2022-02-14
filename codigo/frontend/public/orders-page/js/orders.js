f3858339a3eb6d48b30fe77b214fa20cf815d058
Abigael <abigael-hf@hotmail.com>
2022-02-14 14:41:31 +0100

    orders-page añadida

---
+++ codigo/frontend/public/orders-page/js/orders.js
@@ -0,0 +1,13 @@
+import {
+    User,
+    UserController,
+    Artwork,
+    ArtworkView,
+    ArtworkController,
+    Catalogue,
+    FilterController,
+    FilterView,
+    ShoppingCart,
+} from "../../app/app.js";
+
+$(document).ready(function() {});
